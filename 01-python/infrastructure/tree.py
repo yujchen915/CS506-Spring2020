2f846aae9f6b929f1667ee37738cbf77a0df3ee0
yujchen915 <chenyujingYW@163.com>
2020-01-28 15:44:01 -0500

    add trees to the city

--- 01-python/infrastructure/tree.py
+++ 01-python/infrastructure/tree.py
@@ -1,3 +1,7 @@
 def draw_tree():
-    print("tree not found")
+    print("    /\    ")
+    print("   /  \   ")
+    print("  / || \  ")
+    print("  /_||_\  ")
+    print("    ||    ")
     return
